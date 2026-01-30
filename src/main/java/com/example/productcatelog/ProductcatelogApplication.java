578585b3ee5311a0d0eef518f360c871a875162b
Gayathri-Anandhan <153351016+Gayathri-Anandhan@users.noreply.github.com>
2026-01-30 23:13:38 +0530

    Add files via upload

---
+++ src/main/java/com/example/productcatelog/ProductcatelogApplication.java
@@ -0,0 +1,13 @@
+package com.example.productcatelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcatelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcatelogApplication.class, args);
+	}
+
+}
